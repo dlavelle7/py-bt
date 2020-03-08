704352e5c80f172042e42612952e5d953834c88f
David Lavelle <david.lavelle@genesys.com>
2020-03-08 18:10:47 +0000

    updated readme and finished testing

--- tests/test_model_validation.py
+++ tests/test_model_validation.py
@@ -8,8 +8,31 @@ from bt.behaviour_tree import BehaviourTree
 @patch("bt.behaviour_tree.importlib.import_module")
 class TestValidation(TestCase):
 
-    # TODO: test 2 root fields
-    # safe yaml load
+    @patch("bt.behaviour_tree.BehaviourTree._load_json")
+    def test_root_no_tasks_path(self, mock_load_json, mock_import):
+        invalid_data = {
+            "tree": {"task": "foo"}
+        }
+        mock_load_json.return_value = invalid_data
+        bt = BehaviourTree("foobar.json")
+        with self.assertRaises(ValidationError) as exc_context:
+            bt.load()
+        self.assertEqual(exc_context.exception.message,
+                         "'tasks_path' is a required property")
+        self.assertFalse(mock_import.import_module.called)
+
+    @patch("bt.behaviour_tree.BehaviourTree._load_json")
+    def test_root_no_tree(self, mock_load_json, mock_import):
+        invalid_data = {
+            "tasks_path": "foo.bar"
+        }
+        mock_load_json.return_value = invalid_data
+        bt = BehaviourTree("foobar.json")
+        with self.assertRaises(ValidationError) as exc_context:
+            bt.load()
+        self.assertEqual(exc_context.exception.message,
+                         "'tree' is a required property")
+        self.assertFalse(mock_import.import_module.called)
 
     @patch("bt.behaviour_tree.BehaviourTree._load_json")
     def test_decorator_with_invalid_child_node_key(self, mock_load_json, mock_import):
@@ -28,7 +51,6 @@ class TestValidation(TestCase):
             bt.load()
         self.assertEqual(exc_context.exception.message,
                          "Additional properties are not allowed ('BAD_KEY' was unexpected)")
-        self.assertFalse(mock_import.import_module.called)
 
     @patch("bt.behaviour_tree.BehaviourTree._load_yaml")
     def test_composite_with_invalid_child_node_value(self, mock_load_yaml, mock_import):
@@ -60,4 +82,3 @@ class TestValidation(TestCase):
             bt.load()
         self.assertEqual(exc_context.exception.message,
                          "{'BAD_KEY': 'BAD_VALUE'} is not of type 'string'")
-        self.assertFalse(mock_import.import_module.called)
