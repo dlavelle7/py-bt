69ab4d8cb251126e1fdcab299003d29b003145ac
David Lavelle <david.lavelle@genesys.com>
2020-03-08 18:00:10 +0000

    todos

--- tests/test_model_validation.py
+++ tests/test_model_validation.py
@@ -8,8 +8,11 @@ from bt.behaviour_tree import BehaviourTree
 @patch("bt.behaviour_tree.importlib.import_module")
 class TestValidation(TestCase):
 
+    # TODO: test 2 root fields
+    # safe yaml load
+
     @patch("bt.behaviour_tree.BehaviourTree._load_json")
-    def test_decorator_recursive_error(self, mock_load_json, mock_import):
+    def test_decorator_with_invalid_child_node_key(self, mock_load_json, mock_import):
         invalid_data = {
             "tasks_path": "foo.bar",
             "tree": {
@@ -21,4 +24,40 @@ class TestValidation(TestCase):
         }
         mock_load_json.return_value = invalid_data
         bt = BehaviourTree("foobar.json")
-        self.assertRaises(ValidationError, bt.load)
+        with self.assertRaises(ValidationError) as exc_context:
+            bt.load()
+        self.assertEqual(exc_context.exception.message,
+                         "Additional properties are not allowed ('BAD_KEY' was unexpected)")
+        self.assertFalse(mock_import.import_module.called)
+
+    @patch("bt.behaviour_tree.BehaviourTree._load_yaml")
+    def test_composite_with_invalid_child_node_value(self, mock_load_yaml, mock_import):
+        invalid_data = {
+            "tasks_path": "foo.bar",
+            "tree": {
+                "sequence": [
+                    {
+                        "retry": {
+                            "selector": [
+                                {
+                                    "task": "good_value",
+                                },
+                                {
+                                    "task": {"BAD_KEY": "BAD_VALUE"}
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        "task": "another_good_value"
+                    }
+                ]
+            }
+        }
+        mock_load_yaml.return_value = invalid_data
+        bt = BehaviourTree("foobar.yaml")
+        with self.assertRaises(ValidationError) as exc_context:
+            bt.load()
+        self.assertEqual(exc_context.exception.message,
+                         "{'BAD_KEY': 'BAD_VALUE'} is not of type 'string'")
+        self.assertFalse(mock_import.import_module.called)
